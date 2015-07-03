984b3b17ff503c8f36ad117abc0cba60be9d6d5b
Halfdan Mouritzen <halfdan@robothangarskib.dk>
2015-07-03 14:20:26 +0200

    Initial commit with horrid hardcoded IPs and such

---
+++ SaltoPlayer.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python2
+
+import sys
+import threading, time, json, datetime
+import OSC
+
+class SaltoPlayer():
+    def __init__(self, filename):
+        self.filename = filename
+        self.server_address = '172.16.9.80'
+        self.server_port = 14040
+
+        self.client = OSC.OSCClient()
+        self.client.connect((self.server_address, self.server_port))
+
+    def run(self):
+        print('Replaying recording ...')
+        with open(self.filename, 'r') as thefile:
+            for line in thefile:
+                msg = json.loads(line)
+                oscmessage = OSC.OSCMessage('/salto2/sensor')
+                oscmessage.append(msg)
+                self.client.send(oscmessage)
+
+if len(sys.argv) < 2:
+    print('Please supply filename')
+else:
+    filename = sys.argv[1]
+    sp = SaltoPlayer(filename)
+    sp.run()
---
+++ SaltoRecorder.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python2
+
+import threading, time, json, datetime
+import OSC
+
+# Setup
+
+class SaltoRecorder():
+    def __init__(self):
+        self.bind_address = '0.0.0.0'
+        self.bind_port = 14040
+
+        self.is_receiving = False
+
+        datestr = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+
+        self.writeFile = open(datestr + '.txt', 'w')
+
+
+    def print_interface(self):
+        print('**********************')
+        print('*   Salto Recorder   *')
+        print('**********************')
+        print('')
+        print('Listening on port %d' % (self.bind_port,))
+        print('Waiting ...')
+
+
+    def run(self):
+        self.print_interface()
+        self.setup_server()
+
+    def close(self):
+        self.server.close()
+        self.writeFile.close()
+
+
+    def setup_server(self):
+        self.server = OSC.OSCServer((self.bind_address, self.bind_port))
+        self.server.addMsgHandler('/salto2/sensor', self.recorder_callback)
+        self.st = threading.Thread( target = self.server.serve_forever )
+        self.st.start()
+
+
+    def recorder_callback(self, addr, tags, data, client_address):
+        if not self.is_receiving:
+            print('Server is receiving data')
+            print('Recording ... (Press Ctrl+C to stop)')
+            self.is_receiving = True
+
+        self.writeFile.write(json.dumps(data) + '\n')
+
+
+suit = SaltoRecorder()
+suit.run()
+
+try :
+    while 1 :
+        time.sleep(1)
+
+except KeyboardInterrupt :
+    print("\nClosing Salto Recorder.")
+    suit.close()
+    #st.join()
+    print("Done")
